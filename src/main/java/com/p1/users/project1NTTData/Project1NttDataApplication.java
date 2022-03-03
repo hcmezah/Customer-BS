8a437d4f9a3939f05094007f770cc10cf078fb08
uCrover <chris66xd@gmail.com>
2022-03-03 13:01:15 -0500

    add config server

--- src/main/java/com/p1/users/project1NTTData/Project1NttDataApplication.java
+++ src/main/java/com/p1/users/project1NTTData/Project1NttDataApplication.java
@@ -2,8 +2,10 @@ package com.p1.users.project1NTTData;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Project1NttDataApplication {
 
